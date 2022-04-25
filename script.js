ebffcea6d373682e877bd2867228ca16f06ce9e8
Samir <samirspacetech@gmail.com>
2022-04-25 17:34:37 +0200

    [UPDATE]
    Ajustement du menu accordeon.

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
-var acc = document.getElementsByClassName("qst");
-var i;
+let acc = document.getElementsByClassName("qst");
+let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        this.classList.toggle("qst-bold");
 
 
-})}
 
 
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+
+
+        // acc[i].classList.remove('panel');
+        // this.classList.toggle("panel");
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    })
+}
