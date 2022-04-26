587c85f211fb7c2961765db9e8677b07c297233a
Samir <samirspacetech@gmail.com>
2022-04-26 02:23:11 +0200

    [UPDATE]
    Ajout de medias queries pour le responsive design, ajout du nouveau fond écran pour la version mobile, ajustement des tailles de polices et des divs.

--- script.js
+++ script.js
@@ -1,73 +1,86 @@
 let acc = document.getElementsByClassName("qst");
-let i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        this.classList.toggle("qst-bold");
+        // this.classList.toggle("qst-bold")
+
+        (document.querySelector('.qst-bold')) ? document.querySelector('.qst-bold').classList.remove('qst-bold') : '';
+        this.classList.add('qst-bold');
 
 
+        for (j = 0; j < acc.length; j++) {
+            if (j !== i)
+                acc[j].nextElementSibling.style.maxHeight = null;
 
 
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
         }
-
-
+        var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
-
-
-        // acc[i].classList.remove('panel');
-        // this.classList.toggle("panel");
             panel.style.maxHeight = panel.scrollHeight + "px";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // if (panel.style.display === "block") {
+            //     panel.style.display = "none";
+            // } else {
+            //     panel.style.display = "block";
+            // }
+
+
+
+
+
+
+
+
+
+
+
+            // if (panel.style.maxHeight) {
+            //     panel.style.maxHeight = null;
+            // } else {
+            //     panel.style.maxHeight = panel.scrollHeight + "px";
+            // }
+
+
+
+
+
+
+
+
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     })
 }
